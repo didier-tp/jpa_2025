0ecd2107ff10402b139c1e838a1643f1a2dacd3f
Didier_Defrance <didier@d-defrance.fr>
2025-11-24 19:56:45 +0100

    avec script sql également en version postgresql

---
+++ appliJpa2025/src/script/postgresql/README.txt
@@ -0,0 +1,31 @@
+au sein de la console de postgresql (pgAdmin, ...):
+1) créer une nouvelle base de données (ex: BaseTpJpa2025)
+2) au sein de celle-ci lancer l'interprétation d'un copier/coller du contenu
+du fichier init-db-postgres.sql
+
+=========================
+
+select * from ligne_facture;
+select * from facture;
+select * from client_infos_facture;
+select * from article;
+select * from article_materiel;
+select * from article_numerique;
+
+SELECT * from film;
+SELECT * from acteur;
+SELECT * from acteur_film;
+
+SELECT * FROM devise;
+SELECT * FROM pays;
+
+SELECT * FROM commande;
+SELECT * FROM ligne_commande;
+
+SELECT * FROM employe;
+SELECT * FROM compte;
+SELECT * FROM operation;
+SELECT * FROM client;
+SELECT * FROM adresse_de_client;
+SELECT * FROM client_compte;
+SELECT * FROM resa_avec_version;
--- appliJpa2025/src/script/postgresql/init-db-postgres.sql
+++ appliJpa2025/src/script/postgresql/init-db-postgres.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE IF NOT EXISTS BaseTpJpa2025 ;
-use BaseTpJpa2025;
 
 DROP TABLE IF EXISTS ligne_commande;
 DROP TABLE IF EXISTS commande;
@@ -29,7 +27,7 @@ DROP TABLE IF EXISTS article;
 
 
 create table commande (
-numero integer not null auto_increment,
+numero BIGSERIAL  NOT NULL,
 date date, 
 primary key (numero));
 
@@ -44,14 +42,14 @@ primary key (num_commande, num_ligne));
 ALTER TABLE ligne_commande ADD CONSTRAINT ligne_commande_avec_commande_valide
 FOREIGN KEY (num_commande) REFERENCES commande(numero);
 
-INSERT INTO commande (numero,date) VALUES (1,'2023-12-19');
+INSERT INTO commande (date) VALUES ('2023-12-19');
 INSERT INTO ligne_commande (num_commande,num_ligne,prix_unitaire,quantite,ref_produit) 
  VALUES (1,1,2.4,1,'stylo');
 INSERT INTO ligne_commande (num_commande,num_ligne,prix_unitaire,quantite,ref_produit) 
  VALUES (1,2,6.1,3,'cahier');
 
 CREATE TABLE employe(
-emp_id INTEGER auto_increment,
+emp_id BIGSERIAL  NOT NULL,
 firstname VARCHAR(64),
 lastname VARCHAR(64),
 phone_number VARCHAR(64),
@@ -61,17 +59,17 @@ password VARCHAR(64),
 PRIMARY KEY(emp_id));
 
 CREATE TABLE compte(
-numero INTEGER auto_increment,
+numero BIGSERIAL  NOT NULL,
 label  VARCHAR(64),
-solde DOUBLE,
+solde double precision,
 type_compte VARCHAR(64),
-taux DOUBLE,
+taux double precision,
 PRIMARY KEY(numero));
 
 CREATE TABLE operation(
-num_op INTEGER auto_increment,
+num_op BIGSERIAL  NOT NULL,
 label  VARCHAR(64),
-montant DOUBLE,
+montant double precision,
 date_op  VARCHAR(16),
 num_compte INTEGER,
 PRIMARY KEY(num_op));
@@ -80,7 +78,7 @@ ALTER TABLE operation ADD CONSTRAINT operation_avec_compte_valide
 FOREIGN KEY (num_compte) REFERENCES compte(numero);
 
 CREATE TABLE client(
-id INTEGER auto_increment,
+id BIGSERIAL  NOT NULL,
 prenom  VARCHAR(64),
 nom VARCHAR(64),
 PRIMARY KEY(id));
@@ -107,25 +105,25 @@ ALTER TABLE client_compte ADD CONSTRAINT client_compte_avec_client_valide
 FOREIGN KEY (id_client) REFERENCES client(id);
 
 
-INSERT INTO employe (emp_id,firstname,lastname,phone_number,email,login,password)
-VALUES (1,'alain', 'Therieur' , '0102030405' , 'alain.therieur@xyz.com','login1','pwd1');
+INSERT INTO employe (firstname,lastname,phone_number,email,login,password)
+VALUES ('alain', 'Therieur' , '0102030405' , 'alain.therieur@xyz.com','login1','pwd1');
 
-INSERT INTO employe (emp_id,firstname,lastname,phone_number,email,login,password)
-VALUES (2,'axelle', 'Aire' , '0102030405' , 'axelle.aire@m2i.com','login2','pwd2');
+INSERT INTO employe (firstname,lastname,phone_number,email,login,password)
+VALUES ('axelle', 'Aire' , '0102030405' , 'axelle.aire@m2i.com','login2','pwd2');
 
-INSERT INTO compte (numero,label,solde,type_compte,taux) 
-   VALUES (1,'compteA', 50.0,'CompteCourant',null);
-INSERT INTO compte (numero,label,solde,type_compte,taux) 
-  VALUES (2,'compteB', 70.0,'CompteEpargne',1.2);
+INSERT INTO compte (label,solde,type_compte,taux)
+   VALUES ('compteA', 50.0,'CompteCourant',null);
+INSERT INTO compte (label,solde,type_compte,taux)
+  VALUES ('compteB', 70.0,'CompteEpargne',1.2);
 
 
-INSERT INTO operation (num_op,label,montant,date_op,num_compte) 
-   VALUES (1,'achat bonbons', -5.2 , '2022-12-08' , 1);
-INSERT INTO operation (num_op,label,montant,date_op,num_compte) 
-    VALUES (2,'paye novembre', 2000 , '2022-11-30' , 1);
+INSERT INTO operation (label,montant,date_op,num_compte)
+   VALUES ('achat bonbons', -5.2 , '2022-12-08' , 1);
+INSERT INTO operation (label,montant,date_op,num_compte)
+    VALUES ('paye novembre', 2000 , '2022-11-30' , 1);
     
-INSERT INTO client (id,prenom,nom) VALUES (1,'alex', 'Therieur');
-INSERT INTO client (id,prenom,nom) VALUES (2,'jean', 'Aimare');
+INSERT INTO client (prenom,nom) VALUES ('alex', 'Therieur');
+INSERT INTO client (prenom,nom) VALUES ('jean', 'Aimare');
 
 INSERT INTO client_compte (id_client,num_compte) VALUES (1,1); 
 INSERT INTO client_compte (id_client,num_compte) VALUES (2,1);
@@ -138,26 +136,25 @@ INSERT INTO adresse_de_client (id_client,numero,rue,code_postal,ville)
       VALUES (2,'3bis','rue zz' , '76000' , 'Rouen');      
 
 CREATE TABLE resa_avec_version(
-id_resa INTEGER auto_increment,
+id_resa BIGSERIAL  NOT NULL,
 num_version INTEGER,
 objet  VARCHAR(64),
 num_client INTEGER,
 PRIMARY KEY(id_resa));
 
-INSERT INTO resa_avec_version (id_resa,num_version,objet,num_client) 
-  VALUES (1,1,'uniqueVoitureDeCourtoisie' , null); 
+INSERT INTO resa_avec_version (num_version,objet,num_client)
+  VALUES (1,'uniqueVoitureDeCourtoisie' , null);
   
 CREATE TABLE big_data (
-   id bigint not null auto_increment, 
-   image longblob, 
-   json_data longtext, 
-   primary key (id));  
-   
+   id BIGSERIAL not null,
+   image BYTEA,
+   json_data TEXT,
+   primary key (id));
    
 CREATE TABLE devise(
 	code_devise VARCHAR(8),
 	monnaie VARCHAR(64),
-	d_change double,
+	d_change double precision,
 	PRIMARY KEY(code_devise));	 
 
 CREATE TABLE pays(
@@ -191,25 +188,25 @@ INSERT INTO pays (capitale,code_pays,nom_pays,ref_devise)
                VALUES ('Tokyo','JP','Japon','JPY');  	
    
 CREATE TABLE film
-     ( id_film integer  NOT NULL auto_increment,
+     ( id_film BIGSERIAL  NOT NULL,
        titre VARCHAR(64) ,
        date_sortie date ,
        producteur VARCHAR(48),
        PRIMARY KEY (id_film) );
 
-INSERT INTO film Values (1,'Film1' , '1950-02-23' , 'producteur xx');
-INSERT INTO film Values (2,'Film2' , '1960-02-23' , 'producteur yy');
-INSERT INTO film Values (3,'Film3' , '1970-02-23' , 'producteur zz');
+INSERT INTO film(titre,date_sortie,producteur) Values ('Film1' , '1950-02-23' , 'producteur xx');
+INSERT INTO film(titre,date_sortie,producteur) Values ('Film2' , '1960-02-23' , 'producteur yy');
+INSERT INTO film(titre,date_sortie,producteur) Values ('Film3' , '1970-02-23' , 'producteur zz');
 
 
 CREATE TABLE acteur
-     ( id_acteur integer  NOT NULL auto_increment,
+     ( id_acteur BIGSERIAL  NOT NULL,
        nom VARCHAR(48) ,
        prenom VARCHAR(64),
        PRIMARY KEY (id_acteur) );
 
-INSERT INTO acteur Values (1,'Richard' , 'Pierre' );
-INSERT INTO acteur Values (2,'Depardieu' , 'Gérard' );
+INSERT INTO acteur(nom,prenom) Values ('Richard' , 'Pierre' );
+INSERT INTO acteur(nom,prenom) Values ('Depardieu' , 'Gérard' );
  
 
 CREATE TABLE acteur_film
@@ -230,26 +227,26 @@ INSERT INTO acteur_film(id_acteur,id_film,role) Values (2,2 , 'voleur' );
 INSERT INTO acteur_film(id_acteur,id_film,role) Values (2,3 , 'amoureux' );
                           
 CREATE TABLE article
-     ( id integer  NOT NULL auto_increment,
+     ( id BIGSERIAL  NOT NULL,
        label VARCHAR(64) ,
-       prix double,
+       prix double precision,
        PRIMARY KEY (id) );                          
 
-INSERT INTO article Values (1,'article1',5.5);
-INSERT INTO article Values (2,'article2',6.6);
-INSERT INTO article Values (3,'article3',7.7);
+INSERT INTO article(label,prix) Values ('article1',5.5);
+INSERT INTO article(label,prix) Values ('article2',6.6);
+INSERT INTO article(label,prix) Values ('article3',7.7);
 
 CREATE TABLE article_materiel
      ( id integer,
-       poids double ,
-       volume double,
+       poids double precision ,
+       volume double precision,
        PRIMARY KEY (id) ); 
 	   
 
 ALTER TABLE article_materiel ADD CONSTRAINT complement_materiel_pour_article_valide 
                           FOREIGN KEY  (id) REFERENCES article(id); 
 
-INSERT INTO article_materiel(id,poids,volume) Values (2,1.1,0.2);						  
+INSERT INTO article_materiel(id,poids,volume) Values (2,1.1,0.2);
 	   
 CREATE TABLE article_numerique
      ( id integer,
@@ -263,7 +260,7 @@ ALTER TABLE article_numerique ADD CONSTRAINT complement_numerique_pour_article_v
 INSERT INTO article_numerique(id,download_url,description_url) Values (3,'http://xyz/download/a3','http://xyz/description/a3');							  
 
 CREATE TABLE facture
-     ( numero integer  NOT NULL auto_increment,
+     ( numero BIGSERIAL  NOT NULL,
        date VARCHAR(12) ,
        PRIMARY KEY (numero) );   
        
@@ -274,10 +271,10 @@ CREATE TABLE client_infos_facture(
        email VARCHAR(128) ,
        PRIMARY KEY (num_facture)  );     
 
-INSERT INTO facture Values (1,'2025-05-12');
+INSERT INTO facture(date) Values ('2025-05-12');
 
 CREATE TABLE ligne_facture
-     ( id integer  NOT NULL auto_increment,
+     ( id BIGSERIAL  NOT NULL,
        id_facture integer,
        id_article integer,
        quantite integer default 1,
@@ -296,28 +293,3 @@ INSERT INTO client_infos_facture(num_facture,nom,adresse,email) Values (1,'clien
 INSERT INTO ligne_facture(id_facture,id_article,quantite) Values (1,1,2);
 INSERT INTO ligne_facture(id_facture,id_article,quantite) Values (1,2,3);
 INSERT INTO ligne_facture(id_facture,id_article,quantite) Values (1,3,1);
-
-select * from ligne_facture;
-select * from facture;
-select * from client_infos_facture;
-select * from article;
-select * from article_materiel;
-select * from article_numerique;
-
-SELECT * from film;
-SELECT * from acteur;    
-SELECT * from acteur_film;
-               
-SELECT * FROM devise;
-SELECT * FROM pays;               
-               
-SELECT * FROM commande;
-SELECT * FROM ligne_commande;
-
-SELECT * FROM employe;
-SELECT * FROM compte;
-SELECT * FROM operation;
-SELECT * FROM client;
-SELECT * FROM adresse_de_client;
-SELECT * FROM client_compte;
-SELECT * FROM resa_avec_version;
